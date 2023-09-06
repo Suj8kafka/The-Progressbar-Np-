6ffee3297694ffaf159e210a485079a425716bc5
Sujeet Khadka <96431629+suj8@users.noreply.github.com>
2023-09-06 23:52:24 +0545

    Update j.js

--- j.js
+++ j.js
@@ -102,17 +102,19 @@ const ProgressofDay= document.querySelector('.progressInn')
                      
                     }, 500);          
                     // Retrieve the visitor count from local storage or initialize it to 0
-let count = localStorage.getItem('visitorCount') || 0;
+                    let count = localStorage.getItem('visitorCount') || 0;
 
-// Update the counter element on the page
-const counterElement = document.getElementById('counter');
-counterElement.textContent = count;
+                        // Update the counter element on the page
+                        const counterElement = document.getElementById('counter');
+                        counterElement.textContent = count;
 
-// Increment the visitor count and update the counter
-count++;
-localStorage.setItem('visitorCount', count);
+                        // Increment the visitor count and update the counter
+                        count++;
+                        localStorage.setItem('visitorCount', count);
                     
                     
+                   
+                        //Functions to change colors on time pass by
                     const checkColors = (width)=> { 
                         if (width> 50 && width<80){ 
                             progressBar.style.background= "yellow"; 
