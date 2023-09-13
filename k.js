56a93f2afab58f1c69e7664bdf6f9993f47a1cac
Sujeet Khadka <96431629+suj8@users.noreply.github.com>
2023-09-13 10:20:55 +0545

    Update k.js
    Added Firebase database

--- k.js
+++ k.js
@@ -1,29 +1,70 @@
-     //Google Analytics
-     window.dataLayer = window.dataLayer || [];
-     function gtag(){dataLayer.push(arguments);}
-     gtag('js', new Date());
-
-     gtag('config', 'G-HGQ08F2P9V');
-                         
+// Initialize Firebase (ADD YOUR OWN DATA)
+const firebaseConfig = {
+  apiKey: "AIzaSyC_SF82GQR6edfhZsOkbj1EIES0KB-2PmA",
+  authDomain: "the-progressbar-form-database.firebaseapp.com",
+  databaseURL: "https://the-progressbar-form-database-default-rtdb.firebaseio.com",
+  projectId: "the-progressbar-form-database",
+  storageBucket: "the-progressbar-form-database.appspot.com",
+  messagingSenderId: "702353361774",
+  appId: "1:702353361774:web:024dd39e7e0856f87c8f7f",
+  measurementId: "G-V4P14FTLY4"
+};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDIU4otdbs35G88kNpdJKZIttWTNMd-qGs",
+//     authDomain: "contactform-f0a28.firebaseapp.com",
+//     databaseURL: "https://contactform-f0a28-default-rtdb.firebaseio.com",
+//     projectId: "contactform-f0a28",
+//     storageBucket: "contactform-f0a28.appspot.com",
+//     messagingSenderId: "467495307485",
+//     appId: "1:467495307485:web:1042ec4896a426c12b3334",
+      
+  
+//     };
+  
+  firebase.initializeApp(firebaseConfig);
+  
+  // Reference messages collection
+  var messagesRef = firebase.database().ref('messages');
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  
+  // Submit form
+  function submitForm(e){
+    e.preventDefault();
+  
+    // Get values
+    var email = getInputVal('email');
+    var message = getInputVal('message');
+  
+    // Save message
+    saveMessage(email, message);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+    },3000);
+  
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage( email, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      email:email,
+      message:message
+    });
+  }
  
-     // Import the functions you need from the SDKs you need
-     import { initializeApp } from "firebase/app";
-     import { getAnalytics } from "firebase/analytics";
-     // TODO: Add SDKs for Firebase products that you want to use
-     // https://firebase.google.com/docs/web/setup#available-libraries
 
-     // Your web app's Firebase configuration
-     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-     const firebaseConfig = {
-     apiKey: "AIzaSyC4Liqqt6fX38Z51LGW4TEhgQerWkHbkvY",
-     authDomain: "the-progressbar-np.firebaseapp.com",
-     projectId: "the-progressbar-np",
-     storageBucket: "the-progressbar-np.appspot.com",
-     messagingSenderId: "795499051919",
-     appId: "1:795499051919:web:4d8f64bce8ad1a16b7e9fa",
-     measurementId: "G-Q28PP5HJNX"
-     };
-
-     // Initialize Firebase
-     const app = initializeApp(firebaseConfig);
-     const analytics = getAnalytics(app);
+//In the code above, `'visitorCount'` is the name of the key in the database where you want to store the visitor count.
