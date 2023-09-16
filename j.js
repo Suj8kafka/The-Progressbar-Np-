5d94b135bd6c8ef91590c850bd0dbe52e620e841
Sujeet Khadka <96431629+suj8@users.noreply.github.com>
2023-09-16 15:12:52 +0545

    Update j.js
    RealTime Clock

--- j.js
+++ j.js
@@ -164,5 +164,23 @@ const CurrDay= document.querySelector('.day1');
             var header= document.querySelector("header"); 
             header.classList.toggle("sticky", window.scrollY>0);  
         })
-   
+        function RealTimeClock(){
+            var rtClock= new Date();  
+            var hours= rtClock.getHours(); 
+            var minutes = rtClock.getMinutes(); 
+            var seconds= rtClock.getSeconds(); 
+        
+        // Add AM and PM 
+        var amPm= (hours< 12) ? "AM" : "PM"; 
+        // Convert the hours component to 12-hour format
+        hours= (hours>12 )? hours -12 : hours; 
+        // Pad the hours, minutes and seconds with leading zeros
+        hours= ("0"+ hours).slice(-2); 
+        minutes= ("0" + minutes).slice (-2); 
+        seconds = ("0"+ seconds).slice(-2); 
+        //Display Clock
+        document.getElementById('clock').innerHTML= hours +": "+ minutes+" :"+ seconds+" "+ amPm; 
+        var t= setTimeout(RealTimeClock, 1)
+        }
+        
         
